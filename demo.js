1fe7ef0d88d5c2593cd51c3b377f56d35f44c28b
Billy Ceskavich <billy.ceskavich@voxmedia.com>
2017-12-01 15:23:13 -0500

    Update demo script

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ function loop(times, lastMark, count = 0, countAtLastMark = 0) {
     const now = new Date();
     const diff = now - lastMark;
     if (count !== 0 && diff >= 100) {
-      logProfile(count, countAtLastMark, diff);
+      console.log('WRITING PROFILE LAST 100ms');
       lastMark = now;
       countAtLastMark = count;
     }
@@ -14,31 +14,6 @@ function loop(times, lastMark, count = 0, countAtLastMark = 0) {
       ? Promise.resolve()
       : loop(times, lastMark, count + 1, countAtLastMark);
   });
-}
-
-function logProfile(count, countAtLastMark, diff) {
-  const usage = process.memoryUsage();
-  const heapStats = v8.getHeapStatistics();
-  const heapSpaceStats = v8.getHeapSpaceStatistics();
-  const newSpaceStats = getSpaceStats(heapSpaceStats, 'new_space');
-  const oldSpaceStats = getSpaceStats(heapSpaceStats, 'old_space');
-
-  const info = [
-    'PROFILE',
-    process.uptime() * 1000,
-    count,
-    (count - countAtLastMark) / (diff / 1000),
-    usage.rss,
-    (heapStats.used_heap_size / heapStats.total_heap_size) * 100,
-    (newSpaceStats.space_used_size / newSpaceStats.space_size) * 100,
-    (oldSpaceStats.space_used_size / oldSpaceStats.space_size) * 100
-  ];
-
-  console.log((oldSpaceStats.space_used_size / oldSpaceStats.space_size) * 100);
-};
-
-function getSpaceStats(spaceStats, name) {
-  return spaceStats.find(({ space_name }) => space_name === name);
 }
 
 function demo() {
